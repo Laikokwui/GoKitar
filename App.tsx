8ec5731fce72a68f7c49f0b65db0eb5d5a84df74
Laikokwui <kokwei325@gmail.com>
2025-01-16 22:05:46 +0800

    fix proj

--- App.tsx
+++ App.tsx
@@ -1,13 +1,25 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import * as React from 'react';
-import BottomNav from './src/navigation/BottomNav';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { StyleSheet } from 'react-native';
+import { SafeAreaProvider, initialWindowMetrics } from 'react-native-safe-area-context';
+import { enableScreens } from 'react-native-screens';
+import MainNav from './src/navigations/mainNav';
+
+// Enable screens support before rendering any navigation stack
+enableScreens();
 
 export default function App() {
-  return <BottomNav />;
+  return (
+    <GestureHandlerRootView style={styles.container}>
+      <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+        <MainNav />
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
